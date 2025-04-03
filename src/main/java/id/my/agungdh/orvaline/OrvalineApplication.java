98df9d8e4a36e13f9467af9063933bbd75c7e436
AgungDH <agungdh@live.com>
2025-04-03 11:38:50 +0700

    init

---
+++ src/main/java/id/my/agungdh/orvaline/OrvalineApplication.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.orvaline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrvalineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrvalineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/my/agungdh/orvaline/OrvalineApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.orvaline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrvalineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
